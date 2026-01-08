5297817f827171dac8c2d6da8ece90fffb8bda45
osmanncan <osmancann25@gmail.com>
2026-01-08 22:47:52 +0300

    fix: uygulama adı ve ikon düzeltildi, splash screen güncellendi

--- App.js
+++ App.js
@@ -2,15 +2,13 @@ import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import HomeScreens from './scr/HomeScreens';
-import SplashScreen from './scr/SplashScreen';
 
 const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
    <NavigationContainer>
-      <Stack.Navigator initialRouteName="Splash" screenOptions={{ headerShown: false }}>
-        <Stack.Screen name="Splash" component={SplashScreen} />
+      <Stack.Navigator initialRouteName="Home" screenOptions={{ headerShown: false }}>
         <Stack.Screen name="Home" component={HomeScreens} />
       </Stack.Navigator>
    </NavigationContainer>
