e33de851d3ed1a8a67ce84322abb6d931ac0a72a
osmanncan <osmancann25@gmail.com>
2026-01-11 22:52:45 +0300

    Splash screen entegrasyon: Uygulama açılışında 3 saniye splash screen gösterilecek

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import SplashScreen from './src/SplashScreen';
 import HomeScreens from './src/HomeScreens';
 
 const Stack = createNativeStackNavigator();
@@ -8,7 +9,8 @@ const Stack = createNativeStackNavigator();
 export default function App() {
   return (
    <NavigationContainer>
-      <Stack.Navigator initialRouteName="Home" screenOptions={{ headerShown: false }}>
+      <Stack.Navigator initialRouteName="Splash" screenOptions={{ headerShown: false }}>
+        <Stack.Screen name="Splash" component={SplashScreen} />
         <Stack.Screen name="Home" component={HomeScreens} />
       </Stack.Navigator>
    </NavigationContainer>
