e386293f4257bc9dcde3731b178a2bf4b48b4f8d
osmanncan <osmancann25@gmail.com>
2026-03-01 03:00:02 +0300

    Reklam kurulumu tamamlandi, app-ads.txt eklendi ve versiyon guncellendi

--- App.js
+++ App.js
@@ -1,18 +1,32 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import mobileAds from 'react-native-google-mobile-ads';
 import SplashScreen from './src/SplashScreen';
 import HomeScreens from './src/HomeScreens';
 
 const Stack = createNativeStackNavigator();
 
 export default function App() {
+  useEffect(() => {
+    try {
+      mobileAds()
+        .initialize()
+        .then(adapterStatuses => {
+          console.log('AdMob initialized');
+        })
+        .catch(e => console.log('AdMob init error:', e));
+    } catch (error) {
+      console.log('Note: AdMob not available in this environment');
+    }
+  }, []);
+
   return (
-   <NavigationContainer>
+    <NavigationContainer>
       <Stack.Navigator initialRouteName="Splash" screenOptions={{ headerShown: false }}>
         <Stack.Screen name="Splash" component={SplashScreen} />
         <Stack.Screen name="Home" component={HomeScreens} />
       </Stack.Navigator>
-   </NavigationContainer>
+    </NavigationContainer>
   );
 } 
